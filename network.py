ccee0085fcbfd10962d0e05d05bab19ef9913c21
Chris Cheetham <chris@cheetham.com>
2014-07-22 18:29:11 -0400

    print something utile if run as module

--- network.py
+++ network.py
@@ -8,3 +8,8 @@ class Network(object):
         start = page.find(marker) + len(marker)
         end = page.find("<", start)
         return page[start:end]
+
+
+if __name__ == "__main__":
+    net = Network()
+    print "wan ip:", net.get_wan_ip()
